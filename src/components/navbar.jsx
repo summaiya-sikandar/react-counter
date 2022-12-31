df16bb920bcc5860df6b79b8e45692795b880d70
summaiya-sikandar <summaiyaasikandar@gmail.com>
2022-12-31 22:06:06 +0500

    Es Lint warning resolves

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+    // eslint-disable-next-line 
 import React from 'react';
 
 const Navbar = ({totalCounter}) => {
   return ( 
     <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
-       <a class="navbar-brand" href="#">Navbar
+       <a class="navbar-brand" href='#' >Navbar Menu
           <span className="badge badge-pill badge-secondary">{totalCounter}</span>
        </a>
     </div>
